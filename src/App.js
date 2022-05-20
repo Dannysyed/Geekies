8a06b4f0b97e0e06ab84fe5af92224bb699ad7cb
dannysyed <dannysyed20@gmail.com>
2022-05-20 13:57:26 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
+import Navbar from './components/navbar/navbar';
+import Homepage from './pages/homepage/homepage';
 
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
+
+      <Homepage></Homepage>
       <h1>Geekies</h1>
 
     </div>

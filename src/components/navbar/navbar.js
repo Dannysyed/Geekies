0984dc6e34fccb96d2d8cb7ce30f74ac0ecdf205
itsparasbisht <iamparasbisht@gmail.com>
2022-05-20 20:54:39 +0530

    unwanted

--- src/components/navbar/navbar.js
+++
@@ -1,9 +0,0 @@
-import styles from './navbar.module.css'
-let Navbar = () => {
-    return (
-        <div className={styles.nav}>
-            <p>this is navbar</p>
-        </div>
-    )
-}
-export default Navbar

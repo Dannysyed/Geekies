642d1ba709b7557f229fa702783a31f113843978
dannysyed <dannysyed20@gmail.com>
2022-05-22 14:51:47 +0530

    update navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
 import Navbar from './components/navbar/navbar';
 import Homepage from './pages/homepage/homepage';
-
+import { Switch, Route } from 'react-router-dom'
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+      <Switch>
+        <Route path={'/Homepage'}>
 
-      <Homepage></Homepage>
-      <h1>Geekies</h1>
+          <Homepage></Homepage>
+        </Route>
+      </Switch>
+      {/* <h1>Geekies</h1> */}
 
     </div>
   );
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,8 +1,18 @@
 import styles from './navbar.module.css'
+import { NavLink } from 'react-router-dom'
+import leave from '../../Assets/leave.png'
 let Navbar = () => {
     return (
         <div className={styles.nav}>
-            <p>this is navbar</p>
+            <div className={styles.name}>
+                <h1>Geekies</h1>
+                <img src={leave}></img>
+            </div>
+            <div className={styles.link}>
+                <NavLink to={'/technology'}>Technology</NavLink>
+                <NavLink to={'/Anime'}>Anime</NavLink>
+                <NavLink to={'/Bollywood'}>Bollywood</NavLink>
+            </div>
         </div>
     )
 }

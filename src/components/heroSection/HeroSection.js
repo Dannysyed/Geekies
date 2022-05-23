0dffc67b8f6b11eb2ddf4d6a45533ef784cd167a
itsparasbisht <iamparasbisht@gmail.com>
2022-05-23 13:33:25 +0530

    image tag

--- src/components/heroSection/HeroSection.js
+++ src/components/heroSection/HeroSection.js
@@ -1,9 +1,11 @@
 import React from "react";
 import styles from "./heroSection.module.css";
+import myImage from "../../assets/heroPage/zenitsu.jpg";
 
 function HeroSection({ heading, subHeading }) {
   return (
     <div className={styles.heroContainer}>
+      <img src={myImage} alt="best blogs for today" />
       <h1 className={styles.mainHeading}>{heading}</h1>
       <p className={styles.subHeading}>{subHeading}</p>
     </div>

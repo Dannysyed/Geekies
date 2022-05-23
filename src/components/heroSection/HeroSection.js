825c5955f2f06bc11f1b7d2923cdb6a35e809da5
itsparasbisht <iamparasbisht@gmail.com>
2022-05-23 13:46:27 +0530

    alt

--- src/components/heroSection/HeroSection.js
+++ src/components/heroSection/HeroSection.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { Suspense } from "react";
 import styles from "./heroSection.module.css";
 import myImage from "../../assets/heroPage/zenitsu.jpg";
 
 function HeroSection({ heading, subHeading }) {
   return (
     <div className={styles.heroContainer}>
-      <img src={myImage} alt="best blogs for today" />
+      <Suspense fallback={<h2>loading...</h2>}>
+        <img src={myImage} alt="best blogs to read today" />
+      </Suspense>
       <h1 className={styles.mainHeading}>{heading}</h1>
       <p className={styles.subHeading}>{subHeading}</p>
     </div>

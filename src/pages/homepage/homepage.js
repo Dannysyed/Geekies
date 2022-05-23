1e585746d3d4a2559527014d891135b78847ba68
itsparasbisht <iamparasbisht@gmail.com>
2022-05-23 13:51:02 +0530

    image source

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -1,10 +1,12 @@
 import HeroSection from "../../components/heroSection/HeroSection";
 import styles from "./homepage.module.css";
+import myImage from "../../assets/heroPage/zenitsu.jpg";
 
 let Homepage = () => {
   return (
     <div className={styles.home}>
       <HeroSection
+        imgSrc={myImage}
         heading="Demon Slayer Season 3"
         subHeading="When Demon Slayer season 3 comes out, based on everything we know"
       />

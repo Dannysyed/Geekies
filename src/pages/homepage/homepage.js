4ea3b255298472efd2008d42f0dbf6fe4e11fb1e
itsparasbisht <iamparasbisht@gmail.com>
2022-05-25 15:35:46 +0530

    minor fixes

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -1,13 +1,11 @@
 import HeroSection from "../../components/heroSection/HeroSection";
 import styles from "./homePage.module.css";
-import myImage from "../../Assets/heroPage/zenitsu.jpg";
+import myImage from "../../assets/heroPage/zenitsu.jpg";
 import BlogRows from "../../components/blogRows/BlogRows";
-import Carouselfn from "../../components/heroSection/carusol";
 
 let Homepage = () => {
   return (
     <div className={styles.home}>
-      {/* <Carouselfn></Carouselfn> */}
       <HeroSection
         imgSrc={myImage}
         heading="Demon Slayer Season 3"

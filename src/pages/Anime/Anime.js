2ea469147df2ab0c68eb1445b260e4485d4579a8
itsparasbisht <iamparasbisht@gmail.com>
2022-05-28 12:50:52 +0530

    not required

--- src/pages/Anime/Anime.js
+++
@@ -1,9 +0,0 @@
-import styles from './Anime.module.css'
-let Anime = () => {
-    return (
-        <div className={styles.Anime}>
-            <h1>this is Anime</h1>
-        </div>
-    )
-}
-export default Anime
--- src/pages/Technology/Technology.js
+++
@@ -1,9 +0,0 @@
-import styles from './Technology.module.css'
-let Technology = () => {
-    return (
-        <div className={styles.Technology}>
-            <h1>this is Technology</h1>
-        </div>
-    )
-}
-export default Technology

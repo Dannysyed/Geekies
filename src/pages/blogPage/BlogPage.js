4b0829bee3da55d9059562f0e86d444d3fea0c73
itsparasbisht <iamparasbisht@gmail.com>
2022-05-31 20:20:36 +0530

    file names

--- src/pages/blogPage/BlogPage.js
+++ src/pages/blogPage/BlogPage.js
@@ -1,38 +1,48 @@
-import styles from './blogPage.module.css'
-import penimage from '../../Assets/bg-callout.jpg'
-import penimage1 from '../../Assets/portfolio-3.jpg'
-import penimage2 from '../../Assets/portfolio-2.jpg'
+import styles from "./blogPage.module.css";
+import penimage from "../../assets/bg-callout.jpg";
+import penimage1 from "../../assets/portfolio-3.jpg";
+import penimage2 from "../../assets/portfolio-2.jpg";
 let BlogPage = () => {
-    return (
-        <main>
-            <div className={styles.head}>
-                <img src={penimage}></img>
-            </div>
-            <div className={styles.main_div}>
-                <div className={styles.date}>
-                    <div className={styles.date1}>
-                        <span>June 10, 2019</span>
-                    </div>
-                    <h2>Read This To Change How You Mind</h2>
-                </div>
-                <div className={styles.blog}>
-                    <div className={styles.content}>
-                        <h3>1.  Here goes the Content </h3>
-                        <p>This is the paragrahp for the content in the given below box <br />this was the data in the good form in the ver adasf asfas fasfda sfa sdf</p>
-                    </div>
-                    <div className={styles.content1}>
-                        <img src={penimage1}></img>
-                    </div>
-                    <div className={styles.content}>
-                        <h3>2.  Here goes the Content </h3>
-                        <p>This is the paragrahp for the content in the given below box <br />this was the data in the good form in the ver adasf asfas fasfda sfa sdf</p>
-                    </div>
-                    <div className={styles.content1}>
-                        <img src={penimage2}></img>
-                    </div>
-                </div>
-            </div>
-        </main>
-    )
-}
-export default BlogPage
+  return (
+    <main>
+      <div className={styles.head}>
+        <img src={penimage}></img>
+      </div>
+      <div className={styles.main_div}>
+        <div className={styles.date}>
+          <div className={styles.date1}>
+            <span>June 10, 2019</span>
+          </div>
+          <h2>Read This To Change How You Mind</h2>
+        </div>
+        <div className={styles.blog}>
+          <div className={styles.content}>
+            <h3>1. Here goes the Content </h3>
+            <p>
+              This is the paragrahp for the content in the given below box{" "}
+              <br />
+              this was the data in the good form in the ver adasf asfas fasfda
+              sfa sdf
+            </p>
+          </div>
+          <div className={styles.content1}>
+            <img src={penimage1}></img>
+          </div>
+          <div className={styles.content}>
+            <h3>2. Here goes the Content </h3>
+            <p>
+              This is the paragrahp for the content in the given below box{" "}
+              <br />
+              this was the data in the good form in the ver adasf asfas fasfda
+              sfa sdf
+            </p>
+          </div>
+          <div className={styles.content1}>
+            <img src={penimage2}></img>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+export default BlogPage;

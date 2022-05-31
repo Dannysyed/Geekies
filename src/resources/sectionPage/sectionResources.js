9c8bf04d1781fa2525fcc941c12e42248f3de402
itsparasbisht <iamparasbisht@gmail.com>
2022-05-31 20:57:35 +0530

    resources

--- src/resources/sectionPage/sectionResources.js
+++ src/resources/sectionPage/sectionResources.js
@@ -1,17 +1,17 @@
 const sectionResources = [
   {
-    img: "https://miro.medium.com/max/1000/1*KDMx1YspSrBcFJG-NDZgDg.png",
+    img: "https://www.xda-developers.com/files/2019/10/Microsoft-ARM-powered-Surface_1_1.jpg",
     title:
       "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis recusandae",
     subtitle:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima rem beatae voluptatem hic laudantium ipsam magni eveniet aliquid voluptates cupiditate!",
   },
   {
-    img: "https://miro.medium.com/max/1000/1*KDMx1YspSrBcFJG-NDZgDg.png",
+    img: "https://www.cartoonbrew.com/wp-content/uploads/2022/04/suzumenotojimari-1280x600.jpg",
     title:
-      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis recusandae",
+      "Makoto Shinkai’s Latest Film Suzume no Tojimari to Get International Release Via Crunchyroll!",
     subtitle:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima rem beatae voluptatem hic laudantium ipsam magni eveniet aliquid voluptates cupiditate!",
+      "The latest anime film from renowned director Makoto Shinkai, Suzume no Tojimari, has been picked up for distribution outside of Asia in early 2023",
   },
   {
     img: "https://miro.medium.com/max/1000/1*KDMx1YspSrBcFJG-NDZgDg.png",

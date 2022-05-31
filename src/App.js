862334b6cc3910ae9c1a8eef70bac6c0eb0a5ad4
itsparasbisht <iamparasbisht@gmail.com>
2022-05-31 20:45:11 +0530

    section page props

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HomePage from "./pages/homePage/HomePage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SectionPage from "./pages/sectionPage/SectionPage";
 import BlogPage from "./pages/blogPage/BlogPage";
+import sectionResources from "./resources/sectionPage/sectionResources";
 
 function App() {
   return (
@@ -12,11 +13,56 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/section/technology" element={<SectionPage />} />
-          <Route path="/section/anime" element={<SectionPage />} />
-          <Route path="/section/controversy" element={<SectionPage />} />
-          <Route path="/section/celebs" element={<SectionPage />} />
-          <Route path="/section/productivity" element={<SectionPage />} />
+          <Route
+            path="/section/technology"
+            element={
+              <SectionPage
+                imgSrc={sectionResources[0].img}
+                title={sectionResources[0].title}
+                subtitle={sectionResources[0].subtitle}
+              />
+            }
+          />
+          <Route
+            path="/section/anime"
+            element={
+              <SectionPage
+                imgSrc={sectionResources[1].img}
+                title={sectionResources[1].title}
+                subtitle={sectionResources[1].subtitle}
+              />
+            }
+          />
+          <Route
+            path="/section/controversy"
+            element={
+              <SectionPage
+                imgSrc={sectionResources[2].img}
+                title={sectionResources[2].title}
+                subtitle={sectionResources[2].subtitle}
+              />
+            }
+          />
+          <Route
+            path="/section/celebs"
+            element={
+              <SectionPage
+                imgSrc={sectionResources[3].img}
+                title={sectionResources[3].title}
+                subtitle={sectionResources[3].subtitle}
+              />
+            }
+          />
+          <Route
+            path="/section/productivity"
+            element={
+              <SectionPage
+                imgSrc={sectionResources[4].img}
+                title={sectionResources[4].title}
+                subtitle={sectionResources[4].subtitle}
+              />
+            }
+          />
           <Route path="/blog" element={<BlogPage />} />
           <Route path="*" element={<HomePage />} />
         </Routes>
---
+++ src/resources/sectionPage/sectionResources.js
@@ -0,0 +1,39 @@
+const sectionResources = [
+  {
+    img: "https://miro.medium.com/max/1000/1*KDMx1YspSrBcFJG-NDZgDg.png",
+    title:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis recusandae",
+    subtitle:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima rem beatae voluptatem hic laudantium ipsam magni eveniet aliquid voluptates cupiditate!",
+  },
+  {
+    img: "https://miro.medium.com/max/1000/1*KDMx1YspSrBcFJG-NDZgDg.png",
+    title:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis recusandae",
+    subtitle:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima rem beatae voluptatem hic laudantium ipsam magni eveniet aliquid voluptates cupiditate!",
+  },
+  {
+    img: "https://miro.medium.com/max/1000/1*KDMx1YspSrBcFJG-NDZgDg.png",
+    title:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis recusandae",
+    subtitle:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima rem beatae voluptatem hic laudantium ipsam magni eveniet aliquid voluptates cupiditate!",
+  },
+  {
+    img: "https://www.rollingstone.com/wp-content/uploads/2022/05/met-gala-blake-and-ryan.jpg?w=1024",
+    title:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis recusandae",
+    subtitle:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima rem beatae voluptatem hic laudantium ipsam magni eveniet aliquid voluptates cupiditate!",
+  },
+  {
+    img: "https://miro.medium.com/max/1000/1*KDMx1YspSrBcFJG-NDZgDg.png",
+    title:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis recusandae",
+    subtitle:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima rem beatae voluptatem hic laudantium ipsam magni eveniet aliquid voluptates cupiditate!",
+  },
+];
+
+export default sectionResources;
